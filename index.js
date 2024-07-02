0de8a120c60afa90a53715493560228cfefc0084
Jtn2630 <138483230+Jtn2630@users.noreply.github.com>
2024-07-02 14:08:54 +0000

    Rota /oi

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+//Desafio: criar um endpoint /oi que exibe "Olá, Mundo"
+app.get('/oi', function (req, res) {
+    res.send('Olá Mundo')
+})
+
+
 app.listen(3000)
