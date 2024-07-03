5194b783390002206e841161a6fed07a0a12a7f0
Jtn2630 <138483230+Jtn2630@users.noreply.github.com>
2024-07-03 14:03:37 +0000

    Endpoint Create Finalizado

--- index.js
+++ index.js
@@ -20,4 +20,23 @@ app.get('/item', function (req, res) {
   res.send(lista)
 })
 
+//Sinalizamos para o Express que vamos usar o JSON no Body
+
+app.use(express.json())
+
+//Creat - [POST] /item
+app.post('/item', function (req, res) {
+  //Obtemos o nome enviado no Request Body
+  const item = req.body.nome
+  
+  // Inserimos o item no final da lista
+  lista.push(item)
+
+  //Enviamos uma mensagem de Sucesso
+  res.send('Item Criado com Sucesso!')
+
+
+
+})
+
 app.listen(3000)
